feb631ac8ed666e69e065acd08902bde32797a4a
muhammad mufti ramdhani <56355345+muftiramdhani25@users.noreply.github.com>
2020-10-18 06:58:37 +0700

    fix fix

--- smart_lamp_google_assistant.ino
+++ smart_lamp_google_assistant.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#define BLYNK_PRINT Serial
 
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "***************************"; 
+char ssid[] = " namahostpotmu "; 
+char pass[] = " passwordhostpotmu "; 
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  Blynk.run();
 }
